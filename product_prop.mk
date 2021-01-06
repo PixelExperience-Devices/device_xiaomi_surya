bb95173251c6a1384706b0ea8fdc5677d52422c1
Michael Bestas <mkbestas@gmail.com>
2021-01-06 19:42:24 +0200

    surya: Disable WiFi display for now
    * Don't show the broken setting until we have a working
      QCOM wfd stack
    
    Change-Id: Ie0637914b853609e27e8c5cb5699364b1717f712

--- product_prop.mk
+++ product_prop.mk
@@ -58,5 +58,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # WFD
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
-    persist.debug.wfd.enable=1 \
+    persist.debug.wfd.enable=0 \
     persist.sys.wfd.virtual=0
