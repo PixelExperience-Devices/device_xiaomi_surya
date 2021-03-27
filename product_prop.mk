009224130b82c4d20b94a7e9170df79fae10ff4e
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-03-28 00:29:12 +0300

    surya: product_prop: cleanup
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -24,27 +24,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     vendor.camera.aux.packagelist=com.android.camera,net.sourceforge.opencamera,org.codeaurora.snapcam \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 
-# Charger
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.charger.enable_suspend=true
-
-# Dex2oat
-PRODUCT_PRODUCT_PROPERTIES += \
-     dalvik.vm.dex2oat64.enabled=true
-
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.hwui.renderer=skiavk \
-    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     vendor.display.disable_rotator_downscale=1 \
     ro.surface_flinger.has_wide_color_display=false
-
-# Smart fps switching
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.vendor.smart_dfps.enable=false
 
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -57,10 +43,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.ims.disableDebugLogs=1 \
     persist.vendor.ims.disableIMSLogs=1 \
     persist.vendor.ims.disableQXDMLogs=1
-
-# Priv-app permission
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.control_privapp_permissions=log
 
 # WFD
 PRODUCT_PRODUCT_PROPERTIES += \
