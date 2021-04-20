ae761644e81b955a84c397543aa133267d54978f
Bruno Martins <bgcngm@gmail.com>
2021-04-20 21:56:48 +0100

    surya: Build power-off charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
