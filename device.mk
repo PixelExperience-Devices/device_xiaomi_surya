72caa3ddd6f8b8df79cfa2adc6ed41b38dc5a484
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-04-26 11:00:12 +0530

    surya: Add KeyHandler for side-mounted fingerprint key
    * This prevents the fingerprint sensor from triggering other events.
    
    Change-Id: I2b4543f2f02f974d52e96739569ba4aa9697c77e
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -99,9 +99,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
