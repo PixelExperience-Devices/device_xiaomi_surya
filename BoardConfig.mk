63c414bd39c1b6adcce3e1bc8b50d179262811a4
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-21 10:43:13 +0300

    Revert "surya: switch to boot image v2"
    This reverts commit 1ba44a322e37eed254f30a96d484c6a67327c7f4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,10 +86,6 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := device/surya-caf/kernel
-BOARD_BOOTIMG_HEADER_VERSION := 2
-BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_PREBUILT_DTBIMAGE_DIR := device/surya-caf
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
