3ac3fac0db2764a406751bac8edf94b57ff37151
Arian <arian.kulmer@web.de>
2021-05-30 23:26:35 +0200

    surya: media:Include c2 manifest from media HAL
    Change-Id: I9ea0322af97284ef4b995df185d0c827285c7194
    Signed-off-by: agustkid <agustkid@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
-    $(DEVICE_PATH)/configs/hidl/c2_manifest.xml
+    hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 ODM_MANIFEST_SKUS := surya
 ODM_MANIFEST_SURYA_FILES := \
     $(DEVICE_PATH)/configs/hidl/manifest-qva.xml \
