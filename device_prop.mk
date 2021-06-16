bcf0439413bb503c9090e7a1a7320cff8e49d6eb
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-06-15 21:36:54 -0300

    surya: refresh rate: Add support kernel idle timer
    * Indicates whether Scheduler's idle timer should support a display driver timeout in the kernel.
    * The value of set_idle_timer_ms should be shorter in time than the timeout duration in the kernel.
    
    * Reference 1: https://android.googlesource.com/platform/frameworks/native/+/master/services/surfaceflinger/sysprop/SurfaceFlingerProperties.sysprop
    
    * Reference 2: https://android.googlesource.com/device/google/redfin/+/refs/tags/android-11.0.0_r38/redfin/init.redfin.rc
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device_prop.mk
+++ device_prop.mk
@@ -3,6 +3,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_idle_timer_ms=80
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_display_power_timer_ms=1000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_content_detection_for_refresh_rate=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.support_kernel_idle_timer=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.defer_fps_frame_count=2
