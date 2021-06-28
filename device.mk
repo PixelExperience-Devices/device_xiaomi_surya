47785734f4dc64087f3d5d10a6c9373f5eedb74b
SebaUbuntu <barezzisebastiano@gmail.com>
2021-06-28 12:11:49 +0700

    surya: Move excluded-input-devices.xml to vibrator folder
    * subhajeetmuhuri: also add drv2624:haptics as Present in surya stock
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -413,7 +413,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_sm6150
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vulkan
 PRODUCT_PACKAGES += \
