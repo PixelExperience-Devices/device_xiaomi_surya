756ce4ae606e002260f7ac2b283e41471141127a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-14 18:14:08 +0530

    surya: init: Add support for M2007J20CT model
     * POCO X3 NFC Thailand
     * also drop mod_device prop cuz its unused

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -86,10 +86,12 @@ void vendor_load_properties()
      */
     if (GetProperty("ro.boot.hwname", "") == "karna") { // POCO X3 (India)
         set_device_props("POCO", "karna", "M2007J20CI", "karna_in");
-        property_override("ro.product.mod_device", "surya_in_global");
     } else { // POCO X3 NFC
-        set_device_props("POCO", "surya", "M2007J20CG", "surya_global");
-        property_override("ro.product.mod_device", "surya_global");
+        string hwc = GetProperty("ro.boot.hwc", "");
+        if (hwc == "THAI" || hwc == "THAI_PA") // POCO X3 NFC Thailand
+            set_device_props("POCO", "surya", "M2007J20CT", "surya_global");
+        else // POCO X3 NFC Global
+            set_device_props("POCO", "surya", "M2007J20CG", "surya_global");
     }
 
     // Set hardware revision
