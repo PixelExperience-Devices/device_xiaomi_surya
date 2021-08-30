f107694ef1c0a46ce99926970caca9f18cfb3bcf
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-08-30 01:13:38 -0300

    surya: drop NotchBarKiller and NoCutoutOverlay
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -292,9 +292,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-    KarnaFrameworks \
-    NoCutoutOverlay \
-    NotchBarKiller
+    KarnaFrameworks
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
