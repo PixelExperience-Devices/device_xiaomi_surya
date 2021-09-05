15a3584979963d1b46611574372cc74e14597868
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-09-06 00:01:04 +0700

    surya: parts: Move thermal profiles to its own activity
    Change-Id: Ifbefbafca4caede8ac28f6ddf11abcdb4fc4bd2f
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: CLHEX <clhexftw@protonmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
@@ -22,7 +22,7 @@ import android.view.MenuItem;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 import com.android.settingslib.collapsingtoolbar.R;
 
-public class ThermalActivity extends CollapsingToolbarBaseActivity {
+public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_THERMAL = "thermal";
 
