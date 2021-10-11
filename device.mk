eab949a81ab9584614703eea576fa430872b815d
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-10-11 12:20:07 +0000

    surya: Build vendor.qti.hardware.camera.device@1.0.vendor
        * Fix Camera in A12
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
