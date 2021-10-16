db4bfa40b3190cfd068e100f5c911422569525b2
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-10-16 18:30:26 +0000

    surya: sepolicy: Fix all Selinux denials
       * Selinux denials definitions on Board vendor was not working, so move all these definitions to Board private.
       * Fix new denials.
       * Define Board public too.
    
    - New logs:
    
       * 10-09 21:12:05.194 E/SELinux (550): avc:  denied  { find } for interface=android.hardware.memtrack::IMemtrack sid=u:r:priv_app:s0:c512,c768 pid=19153 scontext=u:r:priv_app:s0:c512,c768 tcontext=u:object_r:hal_memtrack_hwservice:s0 tclass=hwservice_manager permissive=0
    
       * 10-18 11:47:27.010 E/SELinux (560): avc:  denied  { find } for pid=3603 uid=90000 name=content_capture scontext=u:r:isolated_app:s0:c512,c768 tcontext=u:object_r:content_capture_service:s0 tclass=service_manager permissive=0
    
    - Old logs:
    
       * 09-25 20:30:06.492 E/SELinux (547): avc:  denied  { find } for interface=android.hardware.memtrack::IMemtrack sid=u:r:untrusted_app:s0:c247,c256,c512,c768 pid=10573 scontext=u:r:untrusted_app:s0:c247,c256,c512,c768 tcontext=u:object_r:hal_memtrack_hwservice:s0 tclass=hwservice_manager permissive=0
    
       * 06-30 13:55:43.331 E/SELinux (556): avc:  denied  { find } for pid=27523 uid=10294 name=perfshielder scontext=u:r:priv_app:s0:c512,c768 tcontext=u:object_r:default_android_service:s0 tclass=service_manager permissive=0
    
       * 05-20 23:50:11.309 E/SELinux (548): avc:  denied  { find } for interface=android.hardware.memtrack::IMemtrack sid=u:r:gmscore_app:s0:c512,c768 pid=20181 scontext=u:r:gmscore_app:s0:c512,c768 tcontext=u:object_r:hal_memtrack_hwservice:s0 tclass=hwservice_manager permissive=0
    
       * 05-25 18:21:17.612 E/SELinux (546): avc:  denied  { find } for pid=1303 uid=1047 name=nfc scontext=u:r:cameraserver:s0 tcontext=u:object_r:nfc_service:s0 tclass=service_manager permissive=0
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,7 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
