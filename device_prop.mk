ec3bab50fdaea16cbe0e516993e83f5dcaf7d126
Kyle Harrison <khwebmail@gmail.com>
2021-10-21 21:51:12 +0000

    surya: props: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Change-Id: If6430597d8459e62dd78aa458c58237c71618970
    
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device_prop.mk
+++ device_prop.mk
@@ -41,3 +41,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     persist.sys.sf.disable_blurs=1
 endif
+
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
