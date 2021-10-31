29812f723b480ac6ee35d0e0285c54b9735e3732
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-10-31 02:43:40 +0000

    Revert "surya: BoardConfig: IGNORE NEVERALLOWS for now"
    This reverts commit 0a5d311761b2c309ede90da4a9552ba3c3c40364.
    
    Change-Id: I1128789797ecb6791a4092cf6bd17f086876e2ab
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
