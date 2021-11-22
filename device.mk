fb35e3fa6db2f37cc2420d96c8b953b41daed78b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-22 20:52:21 +0530

    surya: Enable bluetooth HAL to read mac address from NV
     * without this, it sets a random mac address
    
    Change-Id: I258d2c7db7dde91b0afc773df14a6209aee398a1

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Bluetooth
 PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.bluetooth.modem_nv_support=true  \
     persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
     persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true \
     persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
