4c1b28f401c2defd3ed1eca19a9af04d6ccf91b9
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-11-25 10:27:28 -0300

    surya: Build more bluetooth libs
          * Fix: 11-24 07:34:47.384 E/a2dp_vendor_ldac_decoder(21311): system/bt/stack/a2dp/a2dp_vendor_ldac_decoder.cc:134 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ia5720b19c29769a244094e6910f89bf36de8f9e3

--- device.mk
+++ device.mk
@@ -73,6 +73,8 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \
     libbthost_if \
+    libldacBT_dec \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
