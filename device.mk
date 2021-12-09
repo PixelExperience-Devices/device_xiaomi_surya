3ff8fab6d6dcfa93f70a42200297335a530623a7
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-12-09 14:33:45 -0400

    surya: overlay: Show "Turbo Charging" again
         * This was left out through a cleanup, why not find it something very significant.
           But users just don't understand that it doesn't change at all and,
           I don't have the patience to explain the same thing over and over again.
    
         * So I'm just going to show this shit again, but more organized this time.
    
         * Revert Partial commit: 1ae51d13cf68b5e9656812cb250a306b81edc90b
    
         * Add Português Brasil strings
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I97d3c7167759ad8c90f75ba2e442dd34acd7f293

--- device.mk
+++ device.mk
@@ -200,7 +200,9 @@ PRODUCT_PACKAGES += \
     NotchBarKiller \
     SuryaFrameworks \
     SuryaSystemUI \
-    SuryaWifiOverlay
+    SuryaWifiOverlay \
+    SuryaChargingOverlay \
+    SuryaSettingsLibOverlay
 
 # ParanoidDoze
 PRODUCT_PACKAGES += \
