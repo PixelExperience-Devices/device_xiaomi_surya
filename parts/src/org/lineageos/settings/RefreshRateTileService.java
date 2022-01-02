7719a43c6847e78a1b826e80ace5fb9c1f2e22b1
Lostark13 <tushar9915@gmail.com>
2022-01-02 10:46:25 +0000

    surya: parts: Cycle through all possible Refresh Rate values
    -adapt for dynamic refresh rate
    -Possible to set min,max refresh rate values same
    -Setting max refresh rate value greater or equal to min refresh rate
    
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I83f1c9be7ee3d79694524cce82e77759e8302a3a
    
    surya: parts: Change refresh rate tile active status
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- parts/src/org/lineageos/settings/RefreshRateTileService.java
+++ parts/src/org/lineageos/settings/RefreshRateTileService.java
@@ -66,14 +66,15 @@ public class RefreshRateTileService extends TileService {
     }
 
     private void cycleRefreshRate() {
-        if (activeRateMin < availableRates.size() - 1) {
-            activeRateMin++;
-        } else {
-            activeRateMin = 0;
+        if(activeRateMax == 0){
+    	    if(activeRateMin == 0){
+                activeRateMin= availableRates.size();
+	    }
+	    activeRateMax = activeRateMin;
+	    float rate = availableRates.get(activeRateMin - 1);
+  	    Settings.System.putFloat(context.getContentResolver(), KEY_MIN_REFRESH_RATE, rate);
         }
-
-        float rate = availableRates.get(activeRateMin);
-        Settings.System.putFloat(context.getContentResolver(), KEY_MIN_REFRESH_RATE, rate);
+        float rate = availableRates.get(activeRateMax - 1);
         Settings.System.putFloat(context.getContentResolver(), KEY_PEAK_REFRESH_RATE, rate);
     }
 
@@ -85,7 +86,7 @@ public class RefreshRateTileService extends TileService {
         displayText = String.format(Locale.US, min == max ? "%d Hz" : "%d - %d Hz", min, max);
         tile.setContentDescription(displayText);
         tile.setSubtitle(displayText);
-        tile.setState(min == max ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setState(min != max ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         tile.updateTile();
     }
 
