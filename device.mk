6542055bfb2243fc712b086dd7ca1dcc16b6803d
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-10 14:54:53 +0000

    surya: parts: Move XiaomiParts permissions and props to system
    [1] - Following our previous change: b4a7b1e50d8d9a2a20265ccfd90e91dc6cf3e805
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ieadd900eaf9d5ee1d2470d880250218b10e609c1

--- device.mk
+++ device.mk
@@ -62,11 +62,11 @@ PRODUCT_PACKAGES += \
     XiaomiParts
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.xiaomiparts.rc \
     $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xiaomiparts.rc \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
 
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     persist.lcd.hbm_mode=0 \
     persist.lcd.cabc_mode=1
 
