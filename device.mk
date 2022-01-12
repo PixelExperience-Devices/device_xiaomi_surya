7f9b397565df6454e7995e1678ed81d127e553e2
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-12 01:31:51 -0400

    surya: Build vendor.qti.hardware.camera.device
    [1] - 01-12 01:06:24.500 E/HidlServiceManagement(12010): Failed to dlopen android.hardware.camera.provider@2.4-impl.so: unknown error
    [2] - 01-12 01:10:44.766 E/LegacySupport(19417): Could not get passthrough implementation for android.hardware.camera.provider@2.4::ICameraProvider/legacy/0.
    [3] - 01-12 01:12:09.842 E/vndksupport(20036): Could not load /vendor/lib64/hw/android.hardware.camera.provider@2.4-impl.so from default namespace: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /vendor/lib64/android.hardware.camera.provider@2.4-legacy.so in namespace (default).
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Idff84f1926798618bd9cef5d3e9728ae2ce077f4

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
