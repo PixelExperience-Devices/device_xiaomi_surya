f8b75a6dffc3338006cb9b978ba8f7fb0701bc2d
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-18 17:56:06 +0000

    surya: Uprev sensors multihal to 2.1
    [1] - This greatly improves the use of Quick tap.
    [2] - Before it was working only sometimes or we had to make strong touches.
    Now it's much more touch sensitive, so it's working better.
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I1bb962218629579a38a988daa6e79ba25dbf3268

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     android.frameworks.sensorservice@1.0 \
     libsensorndkbridge
 
