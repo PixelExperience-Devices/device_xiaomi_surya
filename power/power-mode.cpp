631b809d1fb542fb66e587b5653e96535f8cc32a
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-22 14:05:47 +0000

    surya: power: Introduce DT2W
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Id43ab6c0692d7d35ea34a328b41b246361da792e

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -16,9 +16,43 @@
 
 #include <aidl/android/hardware/power/BnPower.h>
 #include <android-base/file.h>
+#include <android-base/logging.h>
+#include <linux/input.h>
 #include "power-common.h"
 
 #define BATTERY_SAVER_NODE "/sys/module/battery_saver/parameters/enabled"
+
+namespace {
+int open_ts_input() {
+    int fd = -1;
+    DIR *dir = opendir("/dev/input");
+
+    if (dir != NULL) {
+        struct dirent *ent;
+
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_CHR) {
+                char absolute_path[PATH_MAX] = {0};
+                char name[80] = {0};
+
+                strcpy(absolute_path, "/dev/input/");
+                strcat(absolute_path, ent->d_name);
+
+                fd = open(absolute_path, O_RDWR);
+                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
+                    if (strcmp(name, "NVTCapacitiveTouchScreen") == 0)
+                        break;
+                }
+                close(fd);
+                fd = -1;
+            }
+        }
+        closedir(dir);
+    }
+
+    return fd;
+}
+}  // anonymous namespace
 
 namespace aidl {
 namespace android {
@@ -26,10 +60,16 @@ namespace hardware {
 namespace power {
 namespace impl {
 
+static constexpr int kInputEventWakeupModeOff = 4;
+static constexpr int kInputEventWakeupModeOn = 5;
+
 using ::aidl::android::hardware::power::Mode;
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
         case Mode::LOW_POWER:
             *_aidl_return = true;
             return true;
@@ -40,6 +80,21 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            int fd = open_ts_input();
+            if (fd == -1) {
+                LOG(WARNING)
+                    << "DT2W won't work because no supported touchscreen input devices were found";
+                return false;
+            }
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = enabled ? kInputEventWakeupModeOn : kInputEventWakeupModeOff;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+            return true;
+        }
         case Mode::LOW_POWER:
             ::android::base::WriteStringToFile(enabled ? "Y" : "N", BATTERY_SAVER_NODE, true);
             return true;
