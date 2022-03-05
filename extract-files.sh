8f8af6b6d1827725d050a73e2fb1ad286f6a490f
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-05 15:55:48 +0000

    surya: fixup mi_thermald blob
    [1] - 6142b5278fe070bc56e1c7b187aa93c289213696
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/mi_thermald)
+            sed -i "s|ug_cpu|ug_uwu|g" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
