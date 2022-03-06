a8d2587b96f29e1b815f42b24c9cade072870926
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-06 00:42:16 +0000

    Revert "surya: Add KeyHandler for side-mounted fingerprint key"
    This reverts commit 72caa3ddd6f8b8df79cfa2adc6ed41b38dc5a484.

--- device.mk
+++ device.mk
@@ -135,13 +135,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    KeyHandler
-
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 
 # Media
 PRODUCT_COPY_FILES += \
