9fd1d3b6bfa690f65d926d4d108293e83e636987
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-24 22:29:27 +0000

    surya: Drop duplicate egl symlinks
    [1] - https://github.com/AOSPA/android_device_qcom_common/commit/057ad704a3db92b7817329305c0ce3c2bacb4b71
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I455e95dcdad716051671cf80800995ce5f4b5de1

--- Android.mk
+++ Android.mk
@@ -13,25 +13,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 # Symlinks
-EGL_LIBRARIES := \
-    libEGL_adreno.so \
-    libGLESv2_adreno.so \
-    libq3dtools_adreno.so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"
@@ -95,8 +76,6 @@ $(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += \
-    $(EGL_32_SYMLINKS) \
-    $(EGL_64_SYMLINKS) \
     $(RFS_MSM_ADSP_SYMLINKS) \
     $(RFS_MSM_CDSP_SYMLINKS) \
     $(RFS_MSM_MPSS_SYMLINKS) \
