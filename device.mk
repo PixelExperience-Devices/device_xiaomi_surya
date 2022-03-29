bc22b4add81ba7bd0655aff84b8235df26d19867
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-29 19:18:59 +0000

    surya: Checkout drm service.clearkey to 1.3
    [1] - Apparently 1.4 is causing problems to stream netflix content to some tv
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Filesystems table
 PRODUCT_COPY_FILES += \
