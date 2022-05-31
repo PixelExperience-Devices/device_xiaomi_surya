d1e6e606a007362390728a818ee235ed453bd940
xiaowang <xiaowang@codeaurora.org>
2022-05-31 21:30:52 +0900

    surya: props: Enable BT low power adaptive control feature
    High power consumption issues were reported for some HS/CK/Speaker
    in close range(1 meter) on hastings platform as they may not have
    perfect RSSI golden range logic for power control and always request
    SOC to increase TX power to max power level. Newer hastings SOC support
    LPA power control to avoid unnecessary higher power consumption.
    
    The change enable this soc feature according to value of property.
    And disable it for special remote devices which have been added in BL
    when new ACL link is created.
    
    If feature is enabled, controllable power level for remote is L0-L9,
    BT controller increase it to L10 or L11 according to RSSI and NAK rate.
    Otherwise controllable power level for remote is from 0 to max power
    level signified in NVM, most likely L11 for hastings.
    
    CRs-Fixed: 2597191
    Change-Id: I354bb34a84aa7a55537e3a595daac54c46d90603
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.bluetooth.wipower=false \
     vendor.qcom.bluetooth.soc=cherokee
 
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.vendor.btstack.enable.lpa=true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
