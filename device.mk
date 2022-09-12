70bbc074bc85035da83e372cf946936a1436a44f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-12 21:09:48 +0530

    surya: props: Enable ADB by default in non-user builds
    User builds shouldn't have adb enabled by default.
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -400,6 +400,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.mi.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.mi.usb.sh \
     $(LOCAL_PATH)/init/init.qcom.usb.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.usb.sh
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.usb.config=mtp,adb
+endif
+
 # Userspace fastboot
 PRODUCT_PACKAGES += \
     fastbootd
