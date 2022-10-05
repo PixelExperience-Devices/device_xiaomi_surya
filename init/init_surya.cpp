1b04b880bcf9a7dda85a89d2a74f458451e85837
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-10-04 21:13:00 -0400

    surya: init: Rework libinit
    [1] - Based on aospa alioth
    [2] - Make everything simpler
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -1,40 +1,29 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-   Copyright (C) 2021 Paranoid Android.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <cstdlib>
+#include <fstream>
 #include <string.h>
+#include <unistd.h>
+#include <vector>
 
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <android-base/properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -42,57 +31,51 @@
 using android::base::GetProperty;
 using std::string;
 
-void property_override(char const prop[], char const value[])
-{
-    auto pi = (prop_info*) __system_property_find(prop);
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
 
-    if (pi != nullptr)
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void set_ro_build_prop(const string &source, const string &prop,
-                       const string &value, bool product = false) {
-    string prop_name;
-
-    if (product)
-        prop_name = "ro.product." + source + prop;
-    else
-        prop_name = "ro." + source + "build." + prop;
-
-    property_override(prop_name.c_str(), value.c_str());
+void load_poco_karna() {
+    property_override("ro.product.brand", "POCO");
+    property_override("ro.product.device", "karna");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.marketname", "POCO X3");
+    property_override("ro.product.model", "M2007J20CI");
+    property_override("ro.product.name", "karna_in");
 }
 
-void set_device_props(const string brand, const string device,
-        const string model, const string name, const string marketname) {
-    // list of partitions to override props
-    string source_partitions[] = { "", "bootimage.", "odm.", "product.",
-                                   "system.", "system_ext.", "vendor." };
-
-    for (const string &source : source_partitions) {
-        set_ro_build_prop(source, "brand", brand, true);
-        set_ro_build_prop(source, "device", device, true);
-        set_ro_build_prop(source, "product", device, false);
-        set_ro_build_prop(source, "model", model, true);
-        set_ro_build_prop(source, "name", name, true);
-        set_ro_build_prop(source, "marketname", marketname, true);
-    }
+void load_poco_thai() {
+    property_override("ro.product.brand", "POCO");
+    property_override("ro.product.device", "surya");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.marketname", "POCO X3 NFC");
+    property_override("ro.product.model", "M2007J20CT");
+    property_override("ro.product.name", "surya_global");
 }
 
-void vendor_load_properties()
-{
-    /*
-     * Detect device and configure properties
-     */
-    if (GetProperty("ro.boot.hwname", "") == "karna") { // POCO X3 (India)
-        set_device_props("POCO", "karna", "M2007J20CI", "karna_in", "POCO X3");
-    } else { // POCO X3 NFC
-        string hwc = GetProperty("ro.boot.hwc", "");
-        if (hwc == "THAI" || hwc == "THAI_PA") // POCO X3 NFC Thailand
-            set_device_props("POCO", "surya", "M2007J20CT", "surya_global", "POCO X3 NFC");
-        else // POCO X3 NFC Global
-            set_device_props("POCO", "surya", "M2007J20CG", "surya_global", "POCO X3 NFC");
+void load_poco_surya() {
+    property_override("ro.product.brand", "POCO");
+    property_override("ro.product.device", "surya");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.marketname", "POCO X3 NFC");
+    property_override("ro.product.model", "M2007J20CG");
+    property_override("ro.product.name", "surya_global");
+}
+
+void vendor_load_properties() {
+    std::string region = GetProperty("ro.boot.hwc", "");
+    if (region.find("INDIA") != std::string::npos) {
+        load_poco_karna();
+    } else if (region.find("THAI") != std::string::npos) {
+        load_poco_thai();
+    } else {
+        load_poco_surya();
     }
 
     // Set hardware revision
