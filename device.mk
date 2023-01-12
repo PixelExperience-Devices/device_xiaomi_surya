e0e7d9aa5f6fc69d0ed53cab8d4b6a4e45e2d6c4
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-01-12 16:38:45 +0000

    Revert "surya: Migrate to AIDL ClearKey DRM HAL"
    This reverts commit 1ce2f6d8834392da08046c2d31f50bba3c87196e.

--- device.mk
+++ device.mk
@@ -221,7 +221,7 @@ endif
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
