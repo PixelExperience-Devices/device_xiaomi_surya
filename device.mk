2f3b15eb816fff1da2ca3e09700685f42cf4a4cf
Richard Raya <rdxzv.dev@gmail.com>
2023-04-17 00:30:12 -0300

    surya: perf: Import GameOptimizationFeature
    From sm6150 configs of LA.VENDOR.1.0.r1-16000-WAIPIO.QSSI13.0
    
    Change-Id: Ibf391c7244c1cc604925b4239b4dc3219bea5b0a
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -304,6 +304,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/GameOptimizationFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/GameOptimizationFeature.xml \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
