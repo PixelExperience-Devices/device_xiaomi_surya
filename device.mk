300d904e04d1b7bb4f6ee838f8566ee70da75526
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-05-02 02:40:13 +0000

    surya: Update sensormultihal to 2.1
    Change-Id: Iae73b66e827a32f0268e3a6d53e98b73476d58a7
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -315,7 +315,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
