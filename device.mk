72e91b5038c7b0ac4af3c24e7c235b45717bbbbb
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-05-02 02:38:32 +0000

    surya: Build libdng_sdk vendor package
    Change-Id: I795485159e360c267d77ecd55897de4692b7cd8d
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
