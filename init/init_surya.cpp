94d5f1765f85b27991fd732f957852cf0890e7f3
Richard Raya <rdxzv.dev@gmail.com>
2023-05-06 11:03:22 -0400

    surya: init: Fix cts issues based on alioth libinit
     * Running "getprop | grep ro.product.model" on MIUI I found that Xiaomi sets "POCO X3" and "POCO X3 NFC" as model name for karna and surya respectively
     * Remove properties for the Thai variant as it now is the same as global variant
     * Simplify the logic of detecting the Indian variant
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -29,70 +29,39 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstdlib>
-#include <string.h>
+#include <vector>
 
+#include <android-base/logging.h>
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
-#include <android-base/properties.h>
-
-#include "property_service.h"
-#include "vendor_init.h"
 
 using android::base::GetProperty;
-using std::string;
 
-void property_override(string prop, string value)
-{
-    auto pi = (prop_info*) __system_property_find(prop.c_str());
+std::string hwname = GetProperty("ro.boot.hwname", "");
 
-    if (pi != nullptr)
-        __system_property_update(pi, value.c_str(), value.size());
-    else
-        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
+
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void set_ro_build_prop(const string &source, const string &prop,
-                       const string &value, bool product = false) {
-    string prop_name;
-
-    if (product)
-        prop_name = "ro.product." + source + prop;
-    else
-        prop_name = "ro." + source + "build." + prop;
-
-    property_override(prop_name.c_str(), value.c_str());
+void load_poco_karna() {
+    property_override("ro.product.device", "karna");
+    property_override("ro.product.model", "POCO X3");
+    property_override("ro.product.name", "karna_in");
 }
 
-void set_device_props(const string model, const string name, const string marketname,
-                      const string mod_device) {
-    // list of partitions to override props
-    string source_partitions[] = { "", "bootimage.", "odm.", "product.",
-                                   "system.", "system_ext.", "vendor." };
-
-    for (const string &source : source_partitions) {
-        set_ro_build_prop(source, "model", model, true);
-        set_ro_build_prop(source, "name", name, true);
-        set_ro_build_prop(source, "marketname", marketname, true);
-    }
-    property_override("ro.product.mod_device", mod_device.c_str());
-    property_override("bluetooth.device.default_name", marketname.c_str());
-    property_override("vendor.usb.product_string", marketname.c_str());
-}
-
-void vendor_load_properties()
-{
-    // Detect device and configure properties
-
-    if (GetProperty("ro.boot.hwname", "") == "karna") { // POCO X3 (India)
-        set_device_props("M2007J20CI", "karna_in", "POCO X3", "surya_in_global");
-    } else { // POCO X3 NFC
-        string region = GetProperty("ro.boot.hwc", "");
-        if (region == "THAI" || region == "THAI_PA") // POCO X3 NFC Thailand
-            set_device_props("M2007J20CT", "surya_global", "POCO X3 NFC", "surya_global");
-        else // POCO X3 NFC Global
-            set_device_props("M2007J20CG", "surya_global", "POCO X3 NFC", "surya_global");
+void vendor_load_properties() {
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        if (hwname == "karna") {
+            load_poco_karna();
+        }
     }
 
     // Set hardware revision
