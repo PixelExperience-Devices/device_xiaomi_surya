ad3166ecc9e682eb51e67d74e489fbfc54dc78ad
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-15 15:26:26 +0000

    surya: sepolicy: Commonize more sepolicys
    [1] - xiaomi/sepolicy and qcom/common
    
    Change-Id: I87123af7d655c17c0005e17b2e3100d092b580e1
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- sepolicy/surya-sepolicy.mk
+++ sepolicy/surya-sepolicy.mk
@@ -3,7 +3,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/surya/sepolicy/camera/vendor \
     device/xiaomi/surya/sepolicy/common/vendor \
     device/xiaomi/surya/sepolicy/fingerprint/vendor \
-    device/xiaomi/surya/sepolicy/leds/vendor \
     device/xiaomi/surya/sepolicy/power/vendor \
     device/xiaomi/surya/sepolicy/qcom/vendor \
 
